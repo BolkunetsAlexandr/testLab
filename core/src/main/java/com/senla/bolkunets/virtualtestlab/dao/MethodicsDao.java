d1d7b87242da44e1387b444365ad5c8a86e9107c
Bolkunets Alexandr <bolkunets.alexandre.mail@gmail.com>
2018-02-26 12:10:52 +0300

    rename artifact, add interfaces custom dao

---
+++ core/src/main/java/com/senla/bolkunets/virtualtestlab/dao/MethodicsDao.java
@@ -0,0 +1,6 @@
+package com.senla.bolkunets.virtualtestlab.dao;
+
+import com.senla.bolkunets.virtualtestlab.domain.methodics.description.Methodics;
+
+public interface MethodicsDao extends GenericDao<Methodics, Integer> {
+}
---
+++ core/src/main/java/com/senla/bolkunets/virtualtestlab/dao/PassingFactDao.java
@@ -0,0 +1,6 @@
+package com.senla.bolkunets.virtualtestlab.dao;
+
+import com.senla.bolkunets.virtualtestlab.domain.methodics.result.PassingFact;
+
+public interface PassingFactDao extends GenericDao<PassingFact, Integer> {
+}
