e488734f86e0ce131cffddfdd24ec16bf28c8cfd
Bolkunets Alexandr <bolkunets.alexandre.mail@gmail.com>
2018-03-01 14:53:56 +0300

    add test to dal

--- virtualtestlab-dal/src/test/java/com/senla/bolkunets/virtualtestlab/domain/dao/impl/JpaMethodicsDaoTest.java
+++ virtualtestlab-dal/src/test/java/com/senla/bolkunets/virtualtestlab/domain/dao/impl/JpaMethodicsDaoTest.java
@@ -1,5 +1,16 @@
 package com.senla.bolkunets.virtualtestlab.domain.dao.impl;
 
+import com.senla.bolkunets.virtualtestlab.domain.dao.MethodicsDao;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(value = "classpath:spring-test-dao.xml")
 class JpaMethodicsDaoTest {
+
+    @Autowired
+    private MethodicsDao methodicsDao;
 
 }
